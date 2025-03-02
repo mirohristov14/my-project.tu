631e34e008114a9b9ddcb732956d1858b382cda6
Miroslav Hristov <miroslavhristov777@gmail.com>
2025-03-02 11:45:46 +0200

    Update Hall.java

--- src/Hall.java
+++ src/Hall.java
@@ -1,1 +1,31 @@
+public class Hall {
+    private int hallNumber;  // Номер на залата
+    private int rows;        // Брой редове
+    private int seatsPerRow; // Брой места на ред
 
+    // Конструктор
+    public Hall(int hallNumber, int rows, int seatsPerRow) {
+        this.hallNumber = hallNumber;
+        this.rows = rows;
+        this.seatsPerRow = seatsPerRow;
+    }
+
+    // Гетери 
+    public int getHallNumber() {
+        return hallNumber;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getSeatsPerRow() {
+        return seatsPerRow;
+    }
+
+    // Метод за показване на информация за залата
+    @Override
+    public String toString() {
+        return "Зала " + hallNumber + ": " + rows + " реда, " + seatsPerRow + " места на ред.";
+    }
+}
