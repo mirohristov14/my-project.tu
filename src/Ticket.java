d9a007dc8c7792c286e455575e400d536a805f63
Miroslav Hristov <miroslavhristov777@gmail.com>
2025-03-14 17:34:06 +0200

    Добавен клас Ticket за управление на билети
    Добавен е класът Ticket, който съдържа код на билета, ред, място и представление.

---
+++ src/Ticket.java
@@ -0,0 +1,34 @@
+public class Ticket {
+    private String code;
+    private int row;
+    private int seat;
+    private Event event;
+
+    public Ticket(String code, int row, int seat, Event event) {
+        this.code = code;
+        this.row = row;
+        this.seat = seat;
+        this.event = event;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getSeat() {
+        return seat;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    @Override
+    public String toString() {
+        return "Билет " + code + ": Ред " + row + ", Място " + seat + " за " + event.getName() + " на " + event.getDate();
+    }
+}
