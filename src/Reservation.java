eae0a96403431d9ce47d3dd10af0aa1c2a7cc0b0
Miroslav Hristov <miroslavhristov777@gmail.com>
2025-03-21 14:00:55 +0200

    Create Reservation.java
    Добавен клас Reservation за управление на резервации
    
    - Добавен е нов клас `Reservation`, който съдържа информация за ред, място, представление и бележка.
    - Класът включва гетери и метод `toString()` за лесно визуализиране на резервациите.

---
+++ src/Reservation.java
@@ -0,0 +1,35 @@
+public class Reservation {
+    private int row;
+    private int seat;
+    private Event event;
+    private String note;
+
+    public Reservation(int row, int seat, Event event, String note) {
+        this.row = row;
+        this.seat = seat;
+        this.event = event;
+        this.note = note;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getSeat() {
+        return seat;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    @Override
+    public String toString() {
+        return "Резервация: Ред " + row + ", Място " + seat + " за " + event.getName() + " на " + event.getDate() + " (Бележка: " + note + ")";
+    }
+}
+
