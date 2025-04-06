b6b994a1cd947bcede6bed99b21427f237341974
Miroslav Hristov <miroslavhristov777@gmail.com>
2025-04-06 20:23:40 +0300

    Update Hall.java

--- src/Hall.java
+++ src/Hall.java
@@ -1,29 +1,22 @@
-public class Hall {
-    private int hallNumber;  // Номер на залата
-    private int rows;        // Брой редове
-    private int seatsPerRow; // Брой места на ред
+import java.io.Serializable;
 
-    // Конструктор
+public class Hall implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int hallNumber;
+    private int rows;
+    private int seatsPerRow;
+
     public Hall(int hallNumber, int rows, int seatsPerRow) {
         this.hallNumber = hallNumber;
         this.rows = rows;
         this.seatsPerRow = seatsPerRow;
     }
 
-    // Гетери 
-    public int getHallNumber() {
-        return hallNumber;
-    }
+    public int getHallNumber() { return hallNumber; }
+    public int getRows() { return rows; }
+    public int getSeatsPerRow() { return seatsPerRow; }
 
-    public int getRows() {
-        return rows;
-    }
-
-    public int getSeatsPerRow() {
-        return seatsPerRow;
-    }
-
-    // Метод за показване на информация за залата
     @Override
     public String toString() {
         return "Зала " + hallNumber + ": " + rows + " реда, " + seatsPerRow + " места на ред.";
