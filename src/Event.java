d06bfa46b0d634930638d1e9c04aae23264e68b9
Miroslav Hristov <miroslavhristov777@gmail.com>
2025-04-06 20:24:05 +0300

    Update Event.java

--- src/Event.java
+++ src/Event.java
@@ -1,4 +1,9 @@
-public class Event {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Event implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String name;
     private String date;
     private Hall hall;
@@ -9,20 +14,27 @@ public class Event {
         this.hall = hall;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public Hall getHall() {
-        return hall;
-    }
+    public String getName() { return name; }
+    public String getDate() { return date; }
+    public Hall getHall() { return hall; }
 
     @Override
     public String toString() {
         return "Представление: " + name + " на " + date + " в " + hall;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Event other = (Event) obj;
+        return name.equals(other.name) &&
+                date.equals(other.date) &&
+                hall.getHallNumber() == other.hall.getHallNumber();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, date, hall.getHallNumber());
     }
 }
