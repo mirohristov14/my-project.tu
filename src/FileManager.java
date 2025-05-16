99a554e6b616321fe8e63237874e39a4dca4ab94
Miroslav Hristov <miroslavhristov777@gmail.com>
2025-05-16 19:02:21 +0300

    Update FileManager.java

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,12 +1,28 @@
 import java.io.*;
 
+/**
+ * Класът {@code FileManager} отговаря за запазване и зареждане
+ * на обект от тип {@link CinemaData} чрез сериализация във файл.
+ * Използва се за съхраняване на състоянието на кино системата между сесиите.
+ */
 public class FileManager {
+    /** Името на файла, в който се записва или от който се чете обектът. */
     private final String fileName;
 
+    /**
+     * Конструктор, който задава име на файла за работа с данни.
+     *
+     * @param fileName името на файла за сериализация/десериализация
+     */
     public FileManager(String fileName) {
         this.fileName = fileName;
     }
 
+    /**
+     * Записва обект от тип {@link CinemaData} във файл чрез сериализация.
+     *
+     * @param data обектът с данни, който ще се запише
+     */
     public void saveData(CinemaData data) {
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fileName))) {
             out.writeObject(data);
@@ -15,6 +31,11 @@ public class FileManager {
         }
     }
 
+    /**
+     * Зарежда обект от тип {@link CinemaData} от файл чрез десериализация.
+     *
+     * @return зареденият обект или {@code null}, ако възникне грешка или файлът не съществува
+     */
     public CinemaData loadData() {
         File file = new File(fileName);
         if (!file.exists()) return null;
